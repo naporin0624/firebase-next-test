0ef37ad48905963ec5f7bcc61063c5eee9169a31
napori0624 <naporin0624@gmail.com>
2020-09-11 07:00:57 +0900

    :+1: userを取得できるように

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { clientRequest } from "~/utils/request";
 import { findCurrentUser } from "~/store/selector/findCurrentUser";
+import { normalizer } from "~/utils/normalizer";
+import { userEntity } from "~/store/entities/user/model";
 
 import type { NextPage } from "next";
+import type { User } from "~/store/entities/user/model";
+import { currentUserActions } from "~/store/currentUser";
+import { mergeNormalized } from "~/utils/normalizer/mergeNormalized";
 
 const Welcome: NextPage = () => {
   const user = useSelector(findCurrentUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) return;
+    clientRequest()
+      .get<{ me: User }>("/api/me")
+      .then((res) => {
+        const user = res.data.me;
+        const { result, entities } = normalizer(user, userEntity);
+        dispatch(mergeNormalized(entities));
+        dispatch(currentUserActions.put(result));
+      });
+  }, [dispatch, user]);
 
   return (
     <>
